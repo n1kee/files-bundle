f7bf5471215f0330389cb718ef161b2e20d28df1
Your Name <you@example.com>
2023-05-17 10:22:38 +0300

    + mergePath @ FileSystem

--- src/Helper/FileSystem.php
+++ src/Helper/FileSystem.php
@@ -3,12 +3,16 @@
 namespace FilesBundle\Helper;
 
 class FileSystem {
-	static function createPath(string ...$pathParts): string {
+	static function mergePath(string ...$pathParts): string {
 		$pathString = implode(DIRECTORY_SEPARATOR, $pathParts);
 		return preg_replace(
 			"/(\/|\\\){2,}/",
 			DIRECTORY_SEPARATOR,
 			"/{$pathString}"
 		);
+	}
+
+	static function addPath($origin, $path): string {
+		return preg_replace("|^(?!.+:)/?|", "{$origin}/", $path);
 	}
 }
