1f285eb950f15b5ba7acd071600729fe72d2410b
Your Name <you@example.com>
2023-05-19 07:16:50 +0300

    +DEPENDENCIES

--- src/FilesBundle.php
+++ src/FilesBundle.php
@@ -2,7 +2,7 @@
 
 namespace FilesBundle;
 
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
  * Bundle for working with filesystem files.
  *
  */
-class FilesBundle extends AbstractBundle {
+class FilesBundle extends Bundle {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/services.yaml');
